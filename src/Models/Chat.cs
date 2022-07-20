25b0791750141a54271f262bbdebb9605c44173b
DenisYachmenev <42180746+DenisYachmenev@users.noreply.github.com>
2022-07-20 23:53:03 +0700

    Удаление моделей

--- src/Models/Chat.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Chat
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    
-    public virtual ICollection<Message> Messages { get; set; } = new HashSet<Message>();
-    public virtual ICollection<User> Users { get; set; } = new HashSet<User>();
-}
--- src/Models/Status.cs
+++
@@ -1,7 +0,0 @@
-public enum Status : int
-{
-    New = 0,
-    Sent = 1,
-    Read = 2
-}
-
